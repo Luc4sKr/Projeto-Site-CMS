2f55d0c29830f5fe735f7b96a268fbefcf99a659
Mateus Falk <mateus.efalk@gmail.com>
2022-12-20 19:51:47 -0300

    Criar Projeto

---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
