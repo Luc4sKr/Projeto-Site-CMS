f72af3a637388199fa03142efdb4b5d4339d97ac
Mateus Falk <mateus.efalk@gmail.com>
2022-12-20 20:51:19 -0300

    Entities

---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Models/Entities/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models.Entities
+{
+	[Table("categoria")]
+	public class Categoria
+	{
+		[Column("id")]
+		[Display(Name = "Código")]
+		public int Id { get; set; }
+
+		[Column("categoria_nome")]
+		[Display(Name = "Nome Categoria")]
+		public string CategoriaNome { get; set; }
+
+		public virtual ICollection<Conteudo>? Conteudo { get; set; }
+	}
+}
---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Models/Entities/Conteudo.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models.Entities
+{
+	[Table("conteudo")]
+	public class Conteudo
+	{
+		[Column("id")]
+		[Display(Name = "Código")]
+		public int Id { get; set; }
+
+		[Column("nome")]
+		[Display(Name = "Nome")]
+		public string Nome { get; set; }
+
+		[Column("descricao")]
+		[Display(Name = "Descrição")]
+		public string Descricao { get; set; }
+
+		[Column("CategoriaId")]
+		[Display(Name = "Categoria")]
+		public int CategoriaId { get; set;}
+
+		public virtual Categoria? Categoria { get; set; }
+
+		[Column("is_published")]
+		[Display(Name = "Publicado?")]
+		public bool IsPublished { get; set; }
+
+		[Column("is_deleted")]
+		[Display(Name = "Excluido?")]
+		public bool IsDeleted { get; set; }
+
+		public virtual ICollection<Imagem>? Imagem { get; set; }
+	}
+}
