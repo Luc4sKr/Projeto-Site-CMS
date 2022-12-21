00815691964395197b3342af05b49599ea6e4939
Lucas <lucaskreuch77@gmail.com>
2022-12-20 21:50:59 -0300

    update

---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Repository/CategoriaRepository.cs
@@ -0,0 +1,20 @@
+﻿using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models;
+using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models.Entities;
+
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Repository
+{
+    public class CategoriaRepository
+    {
+        private readonly ContextoDatabase _context;
+
+        public CategoriaRepository(ContextoDatabase context)
+        {
+            _context= context;
+        }
+
+        public async Task<IEnumerable<Categoria>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Services/Implements/CategoriaService.cs
@@ -0,0 +1,21 @@
+﻿using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models.Entities;
+using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Repository;
+using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Services.Interfaces;
+
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Services.Implements
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly CategoriaRepository _repository;
+
+        public CategoriaService(CategoriaRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<IEnumerable<Categoria>> GetAllCategorias()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
