5ad665500419948e73db3511f6d7077fff7e8050
Mateus Falk <mateus.efalk@gmail.com>
2022-12-20 23:35:41 -0300

    Controller Conteudo

---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Controllers/CategoriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Controllers
+{
+	public class CategoriaController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Services/Implements/ConteudoService.cs
@@ -0,0 +1,20 @@
+﻿using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Models.Entities;
+using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Repository;
+using Devs2Blu.ProjetosAula.Projeto_Site_CMS.Services.Interfaces;
+
+namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Services.Implements
+{
+	public class ConteudoService : IConteudoService
+	{
+		private readonly ConteudoRepository _repository;
+		public ConteudoService(ConteudoRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<IEnumerable<Conteudo>> GetAllConteudos()
+		{
+			return await _repository.GetAll();
+		}
+	}
+}
