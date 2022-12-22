bee54d78b0f465f8a9bbe9b8e0f9113c6b2b4811
unknown <coramori@gmail.com>
2022-12-21 22:06:57 -0300

    Details | Conteudo

--- Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Repository/ConteudoRepository.cs
+++ Projeto-Site-CMS/src/Devs2Blu.ProjetosAula.Projeto-Site-CMS/Repository/ConteudoRepository.cs
@@ -31,8 +31,10 @@ namespace Devs2Blu.ProjetosAula.Projeto_Site_CMS.Repository
 
 		public async Task<Conteudo> DetailsConteudo(int? id)
 		{
-			return await _context.Conteudo.FirstOrDefaultAsync(conteudo => conteudo.Id == id);
-		}
+            return await _context.Conteudo
+                .Include(conteudo => conteudo.Categoria)
+                .FirstOrDefaultAsync(conteudo => conteudo.Id == id);
+        }
 		#endregion
 
 	}
